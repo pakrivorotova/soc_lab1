080ac322224339f55cab8bae133d040b0fc1c0fe
Polina Krivorotova <53863755+pakrivorotova@users.noreply.github.com>
2021-10-30 15:02:32 -0700

    added comments

--- C_code.c
+++ C_code.c
@@ -6,65 +6,73 @@ const int N = 4;
 #define STACK_OVERFLOW  -100
 #define STACK_UNDERFLOW -101
 
-
-
+// Structure of stack
 typedef struct Stack_tag {
     int data[STACK_MAX_SIZE];
     int size;
 } Stack_t;
- 
-void push(Stack_t *stack, int value) {
-    if (stack->size >= STACK_MAX_SIZE) {
-        exit(STACK_OVERFLOW);
+
+// Function for adding values to stack 
+void push (Stack_t *stack, int value) {
+    if (stack -> size >= STACK_MAX_SIZE) {
+        exit (STACK_OVERFLOW);
     }
-    stack->data[stack->size] = value;
-    stack->size++;
+    stack -> data[stack -> size] = value;
+    stack -> size++;
 }
 
+// Function for adding values to stack 
 int pop(Stack_t *stack) {
-    if (stack->size == 0) {
+    if (stack -> size == 0) {
         exit(STACK_UNDERFLOW);
     }
-    stack->size--;
-    return stack->data[stack->size];
+    stack -> size--;
+    return stack -> data[stack -> size];
 }
 
+// Function for searching for indexes of max element of array
 void find_max_ind (Stack_t stack, int N, int* max_x, int* max_y) {
-	int count = stack.size;
-	int max, temp;
-	max = pop(&stack);
-	*max_x = count-1;
+	int count = stack.size;		// Count = number of last element of array in stack
+	int max, temp;				// max value in array, temporary value
+	max = pop (&stack);			// max = last element of array
+	*max_x = count-1;			// max_x = index of max element in stack (not in array!)
+	
+	// cycle for all array elements in stack from last to the first elements of array:
 	while (stack.size) {
-		count--;
-        temp = pop(&stack);
-		if (temp > max) {
-            max = temp;
-            *max_x = count-1;
+		count--;			// number of next element in stack
+        temp = pop(&stack);	// next element from stack
+		if (temp > max) {	// if temporary element is bigger than max
+            max = temp;		// save temp as max
+            *max_x = count-1;	//save max index in stack as max_x 
         }
 	}
-	*max_y = *max_x % N;
-    *max_x = *max_x / N;
+	*max_y = *max_x % N;	// Count index y as remainder of division index in stack by N
+    *max_x = *max_x / N;	// Count index y as rthe whole part of the division index in stack by N
 }
 
 int main() {
-	int max_x, max_y;
-	int mass[] = 
-	{1000, 8, 30, 20,
-	400, 20, 15, 6,
-	6, 40, 12, 7};
-	Stack_t my_stack;
+	int max_x, max_y;	// indexes x and y of max element
+	int mass[] = 		// array with data
+	{1, 8, 3, 2,
+	4, 2, 5, 6,
+	6, 4, 2, 7};
+	Stack_t my_stack;	// Initiation of stack
 
-	printf("1 Program Start\n");
-	
+	printf("1. Program Start\n");
+
+	// Push array elements to stack (last element will be on the top of stack)
 	for (int i=0; i<M; i++){
 		for (int j=0; j<N; j++) {
 			push (&my_stack, mass[i*N+j]);
 		}
 	}
-	printf("2 Array was pushed in stack\n");
+	printf("2. Array was pushed in stack\n");
 	
+	// Call function to find indexes x and y of max element in array
     find_max_ind (my_stack, N, &max_x, &max_y);
-	printf("Max = %d\nIndex: (%d, %d)", mass[max_x*N+max_y],max_x, max_y);
+
+	// Results output
+	printf("Index of max element: (%d, %d)", max_x, max_y);
 
 	return 0;
 }
--- code.asm
+++ code.asm
@@ -4,149 +4,98 @@ N:
 M:
 .word 3 #number of rows
 matrix:
-.word 1, 8, 3, 20
-.word 4, 2, 5, 6
+.word 1, 8, 3, 2
+.word 4, 20, 5, 6
 .word 6, 4, 2, 7
 
 result:
-.word 1, 1
+.word -1, -1
 
 .text 
 main:
-la t0, M        #t0 = addr M
-lw t4, 0(t0)    #t4 = M
-la t0, N        #addr N
-lw t2, 0(t0)    #t2 = N 
-mul a1, t4, t2  #a1 = size of matrix
+la t0, M        # t0 = addr M
+lw t4, 0(t0)    # t4 = M
+la t0, N        # t0 = addr N
+lw t2, 0(t0)    # t2 = N 
+mul a1, t4, t2  # a1 = size of matrix
 
-la a2, matrix       #a2 = addr of first matrix
-addi a3, x0, 4
+la a2, matrix       # a2 = address of first matrix
+addi a3, x0, 4      
 mul a3, a3, a1
-add a3, a3, a2      #addr of the end of matrix 
-call push_matrix    #function of pushing matrix to the stack
-addi sp, sp, -4     #shift stack pointer
-sw t2, 0(sp)        #push N in the stack
-addi sp, sp, -4     #shift stack pointer
-sw t4, 0(sp)        #push M in the stack
+add a3, a3, a2      # a3 = address of the end of matrix 
+call push_matrix    # function of pushing matrix to the stack
+addi sp, sp, -4     # shift stack pointer
+sw t2, 0(sp)        # push N in the stack
+addi sp, sp, -4     # shift stack pointer
+sw t4, 0(sp)        # push M in the stack
 
+call find_index_of_max  #call function
 
-call find_index_of_max
+la t0, result       # load addr of first memory address of result 
+sw a0, 0(t0)        # store index x of max element
+addi t0, t0, 4      # move to next memory address of result
+sw a1, 0(t0)        # store index y of max element
+addi a3, t0, -4     # a3 = address of first result value
 
-la t0, result
-sw a0, 0(t0)
-addi t0, t0, 4
-sw a1, 0(t0)
-addi a3, t0, -4
-call print
-call exit
+call print          # function to print result
+call exit           # function to exit program
 
-###################################
+#Functions
+
 push_matrix:
-addi, sp, sp, -4    #move stack pointer to next mem addr
-lw t0, 0(a2)        #t0 = current element of matrix
-sw t0, 0(sp)        #store element of matrix in stack
-addi a2, a2, 4      #a2 = addr of next element of matrix
-#lw a0, 0(sp)
-bne a2, a3, push_matrix #if a2 not in the end of matrix then contine
-ret
+addi, sp, sp, -4        # move stack pointer to next mem addr
+lw t0, 0(a2)            # t0 = current element of matrix
+sw t0, 0(sp)            # store element of matrix in stack
+addi a2, a2, 4          # a2 = addr of next element of matrix
+bne a2, a3, push_matrix # if a2 not in the end of matrix then contine
+ret                     # back to main funct
 
 find_index_of_max:
-lw t0, 0(sp)    #t0 - counter of rows from M to 0
-addi sp, sp, 4  #move stack pointer to next element
-lw t6, 0(sp)    #t6 - counter of colunms from N to 0
-add t2, x0, t6  #t2 = N (save N value to restore for each new column) 
-addi sp, sp, 4  #move stack pointer to next element
-#a0 and a1 will be resulted indexes x and y
-addi a0, t0, -1  #a0 = M-1 - index_x of last matrix element
-addi a1, t6, -1  #a1 = N-1 - index_y of last matrix element
-addi t6, t6, -1 #t6 = N-1 - next column
-#call cycle_row
-#ret
-beq x0, t6, next_row    #if colunm number is 0 change row number
+lw t0, 0(sp)    # t0 - counter of rows from M to 0
+addi sp, sp, 4  # move stack pointer to next element
+lw t6, 0(sp)    # t6 - counter of colunms from N to 0
+add t2, x0, t6  # t2 = N (save N value to restore for each new column) 
+addi sp, sp, 4  # move stack pointer to next element
+    #at a0 and a1 will be resulted indexes x and y
+addi a0, t0, -1  # a0 = M-1 - index_x of last matrix element
+addi a1, t6, -1  # a1 = N-1 - index_y of last matrix element
+addi t6, t6, -1  # t6 = N-1 - next column
+beq x0, t6, next_row    # if colunm number is 0 than change row number
+
 cycle_row:
-lw t4, 0(sp)            #t4 = max value, sp on the same place
-addi sp, sp, 4          #move stack pointer to next element
-lw t5, 0(sp)            #t5 = current value
-#check if t5>t4
-bge t5, t4, change_ind  #if current > max value, change index
-sw t4, 0(sp)            #push max (t4)
-addi t6, t6, -1         #t6 = next column 
-#jal cycle_row
+lw t4, 0(sp)            # t4 = max value, sp on the same place
+addi sp, sp, 4          # move stack pointer to next element
+lw t5, 0(sp)            # t5 = current value
+    #check if t5>t4
+bge t5, t4, change_ind  # if current > max value, change index
+sw t4, 0(sp)            # push max (t4)
+addi t6, t6, -1         # t6 = next column 
 bne x0, t6, cycle_row
 next_row:
-addi t0, t0, -1         #t0 = number of next row
-add t6, t2, x0          #t6 = N
-blt x0, t0, cycle_row   #if t > 0 continue  
+addi t0, t0, -1         # t0 = number of next row
+add t6, t2, x0          # t6 = N
+blt x0, t0, cycle_row   # if t > 0 continue  
 ret
 
 change_ind:
-mv a0, t0           #a0 = current row
-addi a0, a0, -1     #a0 = index of current row
-mv a1, t6           #a1 = current column
-addi a1, a1, -1     #a1 = index of current column
-sw t5, 0(sp)        #push max (t5)
-addi t6, t6, -1     #t6 = next column 
-#jal cycle_row
-bne x0, t6, cycle_row
-beq x0, t6, next_row 
-
-
-
-#jal cycle_row
-
-###################################
+mv a0, t0               # a0 = current row
+addi a0, a0, -1         # a0 = index of current row
+mv a1, t6               # a1 = current column
+addi a1, a1, -1         # a1 = index of current column
+sw t5, 0(sp)            # push max (t5)
+addi t6, t6, -1         # t6 = next column 
+bne x0, t6, cycle_row   # if current column is not 0 than continue row
+beq x0, t6, next_row    # if current column is 0 than move to next row
 
 print:
-addi a0, x0, 1 # print_int ecall
-lw a1, 0(a3)
-ecall
-addi a3, a3, 4
-lw a1, 0(a3)
-ecall 
-ret
+addi a0, x0, 1  # print_int ecall
+lw a1, 0(a3)    # load to a1 value from first address of result (index x)
+ecall           # printing index x
+addi a3, a3, 4  # move to the next address of result
+lw a1, 0(a3)    # load to a1 index y
+ecall           # printing
+ret             # back to main function
 
 exit:
-addi a0, x0, 10
-ecall
-
-
-# lw t1, 0(a2)
-
-# addi t2, x0, 0
-# print_cycle:
-# addi a0, x0, 1 # print_int ecall
-# lw a1, 0(a3)
-# ecall
-
-# addi a0, x0, 11 # print_char ecall
-# addi a1, x0, 32
-# ecall
-
-# addi t2, t2, 1
-# addi a3, a3, 4
-# blt t2, t1, print_cycle
-# ret
-
-
-
-# process:
-# lw t1, 0(a2) # N
-# lw t2, 0(a3) # M
-
-# addi t4, x0, 0 # row pointer
-
-# init:
-# addi t5, x0, 0 # result to save
-# addi t3, x0, 0 # row element number
-
-# cycle:
-# lw t6, 0(a4) # load value from array
-# addi a4, a4, 4 # increment array pointer
-# add t5, t5, t6 # count sum
-# addi t3, t3, 1
-# blt t3, t1, cycle
-# sw t5, 0(a5) # save result to resulting array
-# addi a5, a5, 4
-# addi t4, t4, 1
-# blt t4, t2, init
-# ret
+addi a0, x0, 10 # exit ecall
+ecall           # exit
